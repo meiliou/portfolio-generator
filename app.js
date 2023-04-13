d698f9ffce8c1ef76f2a0fd2b9806e50dfe1334a
meiliou <83768277+meiliou@users.noreply.github.com>
2023-04-13 20:10:47 +0400

    errors galore

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-const generatePage = require('./src/page-template.js');
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+
+const inquirer = require('inquirer');
+// import inquirer from 'inquirer';
+
+console.log(inquirer)
 
