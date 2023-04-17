9b9ff585c139981a3768fb7ce3076bf63c3453b4
meiliou <83768277+meiliou@users.noreply.github.com>
2023-04-17 14:02:59 +0400

    project section HTML

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// const fs = require('fs');
+const fs = require('fs');
 // const profileDataArgs = process.argv.slice(2);
 // const [name, github] = profileDataArgs;
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
 // fs.writeFile('./index.html', generatePage(name, github), err => {
 //     if (err) throw new Error(err);
@@ -138,5 +138,12 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  });
+    const pageHTML = generatePage(portfolioData);
+
+      fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+
+      //   console.log('Page created! Check out index.html in this directory to see it!');
+      });
+    });
+
